5aaa3b1efc9b8d8c0f50d58942ad83f600029fa7
Colin James Nguyen <nguyen_c9@denison.edu>
2025-07-02 22:45:06 +0000

    Add comment additions

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /* Task 1 - Complete the function according to the TODO */
-function addComment(username, comment, addToStart) {}
+function addComment(username, comment, addToStart) {
+  const combinedComment = `${username}: ${comment}`;
+
+  if (addToStart === true){
+    // If addToStart is true, add the comment to the start of the comments array
+    comments.unshift(combinedComment);
+  } else {
+    comments.push(combinedComment);
+  }
+
+}
 
 /* Task 2 - Create your showWinnerMessage below according to the TODO */
 
@@ -25,3 +35,26 @@ function filterList(searchTerm, searchUsers) {}
 /* Level Up - Task 9 - Compelte the task according to the TODO */
 
 /* Level Up - Task 10 - Add to the `addComment` function so that the an `@` sign is added to the username if there is not already one before it gets pushed into the array.  */
+
+// Get the button that submits the comment (assuming it has id 'submit-comment')
+const submitButton = document.getElementById("submit-comment");
+
+// Add an event listener to the button
+if (submitButton) {
+  submitButton.addEventListener("click", function () {
+    // Get the username from the input
+    const usernameInput = document.getElementById("new-username");
+    // Get the comment from the input
+    const commentInput = document.getElementById("new-comment");
+    // Get the checkbox value (checked or not)
+    const addToStartCheckbox = document.getElementById("add-to-Start");
+
+    // Get the values from the inputs
+    const username = usernameInput ? usernameInput.value : "";
+    const comment = commentInput ? commentInput.value : "";
+    const addToStart = addToStartCheckbox ? addToStartCheckbox.checked : false;
+
+    // Call the addComment function with the values
+    addComment(username, comment, addToStart);
+  });
+}
