89f472c770455af4e95a9504ccc50a418d4e8897
Colin James Nguyen <nguyen_c9@denison.edu>
2025-07-02 23:17:23 +0000

    Co-authored-by: Luke Langan <Subutaia@users.noreply.github.com> Co-authored-by: EmiTor1144 <emily.torres05@utrgv.edu> Co-authored-by: Vivian Teo <bobbyvivian@users.noreply.github.com> Co-authored-by: Bryce Kline <kline2bm@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,27 +2,50 @@
 function addComment(username, comment, addToStart) {
   const combinedComment = `${username}: ${comment}`;
 
-  if (addToStart === true){
+  if (addToStart === true) {
     // If addToStart is true, add the comment to the start of the comments array
     comments.unshift(combinedComment);
   } else {
     comments.push(combinedComment);
   }
-
 }
 
 /* Task 2 - Create your showWinnerMessage below according to the TODO */
+function showWinnerMessage(message) {
+  const modalBody = document.getElementById("winnerModalBody");
+  // Set the winner message (use template literals for HTML)
+  modalBody.innerHTML = message;
 
+  // Show the Bootstrap modal
+  const winnerModal = new bootstrap.Modal(
+    document.getElementById("winnerModal")
+  );
+  winnerModal.show();
+  showRandomEmoji();
+}
 /* Task 3 - Create your pickWinner below according to the TODO */
+function pickWinner() {
+  const randomIndex = Math.floor(Math.random() * comments.length);
+  const winnerComment = comments[randomIndex];
+  const message = `🎉 The winner is: <strong>${winnerComment}</strong> 🎉`;
+  showWinnerMessage(message);
+}
 
 /* Task 4 - Complete the function according to the TODO */
-function showRandomEmoji() {}
+function showRandomEmoji() {
+  const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+  document.getElementById("winner-emoji").innerText = emoji;
+}
 
 /* Task 5 - Complete the function according to the TODO */
-function reverseOrder() {}
+function reverseOrder() {
+  comments.reverse();
+}
 
 /* Task 6 - Complete the function according to the TODO */
-function removeComment(index) {}
+function removeComment(index) {
+  comments.splice(index, 1);
+}
 
 /* Task 7 - Complete the function according to the TODO */
 function filterEmojiComments() {}
@@ -32,7 +55,7 @@ function filterEmojiComments() {}
 /* Level Up - Task 8 - Complete the filterList function according to the TODO */
 function filterList(searchTerm, searchUsers) {}
 
-/* Level Up - Task 9 - Compelte the task according to the TODO */
+/* Level Up - Task 9 - Complete the task according to the TODO */
 
 /* Level Up - Task 10 - Add to the `addComment` function so that the an `@` sign is added to the username if there is not already one before it gets pushed into the array.  */
 
